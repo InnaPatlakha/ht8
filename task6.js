16f40fbfe8b7365827e03454a681d7fc11f1e6ce
Inna Patlakha <i.inna1903@gmail.com>
2023-07-20 01:59:29 +0300

    task6 done

---
+++ task6.js
@@ -0,0 +1,10 @@
+// Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
+
+let sumI = 0;
+let aver 
+
+for (let i = 1; i <= 500; i++) {
+  sumI = sumI + i;
+  aver = sumI / 500;
+}
+alert(aver);
